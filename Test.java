fd76c21117fba9e835c9b61afc9cf9184a34580a
Eric <ettrudell17@gmail.com>
2022-11-29 20:02:15 -0700

    first commit

---
+++ Test.java
@@ -0,0 +1,7 @@
+public class Test{
+    public static void main(String[] args){
+        System.out.println("My name is Eric");
+        System.out.println("I am 3000 years old");
+        System.out.println("My hometown is Montrose, Colorado");
+    }
+}
